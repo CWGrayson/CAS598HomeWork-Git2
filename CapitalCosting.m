992edd6616dbc99ab877625d00c233567814a028
CWGrayson <cwgrayso@asu.edu>
2024-10-30 09:02:00 -0700

    Error Correction and adding Plotting
    Resolved a calculation error by dividing the interest payment across 730 time intervals to correctly calculate the value. 
    
    Added in the plotting function and tittles.

--- CapitalCosting.m
+++ CapitalCosting.m
@@ -1,10 +1,14 @@
 %% Creating Capital Cost Curves
 
-t = 1:365*5;
-tdisplay = linspace(0,5,365*5); % Creaes the X Axis option
+tdisplay = linspace(0,5,365*5); % Creaes the time dependent variable for both performing math and creating the plotting X Axis option
 
 factoryCost = 10000000; %Raw cost of the factory
 factoryConstructionTime = 2; % Construction time for the factory
 constructionIntrestRate = 0.07;
 
-capitalRequirments = ((factoryCost/factoryConstructionTime)*tdisplay(1:(365*2)))+factoryCost*exp(constructionIntrestRate*tdisplay(1:365*2));
+capitalRequirments = ((factoryCost/factoryConstructionTime)*tdisplay(1:(365*2)) +(factoryCost*exp(constructionIntrestRate*tdisplay(2)))/(365*2));
+
+plot(tdisplay(1:(365*2)),capitalRequirments)
+xlabel("Time, Years");
+ylabel("Capital Requirments, $")
+title("Capital Requirments")
